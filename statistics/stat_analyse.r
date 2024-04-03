dc9afa1296890d58af2c1138a7405b92e2941397
Exagor <alexandre.achten@gmail.com>
2024-04-03 22:59:15 +0200

    stats of best improv

--- statistics/stat_analyse.r
+++ statistics/stat_analyse.r
@@ -1,9 +1,19 @@
-results <- read.csv("results/results_complete.csv", header = TRUE)
+results <- read.csv("results/results_best.csv", header = TRUE)
 
-# Aggregate by permutation and calculate mean cost and time
-permutation_aggregate <- aggregate(cbind(cost, time) ~ permutation, data = results, FUN = mean)
-permutation_aggregate
+# Aggregate by permutation and calculate mean cost and time for size = 250
+permutation_aggregate_250 <- aggregate(cbind(relative_deviation, time) ~ permutation, data = subset(results, size == 250), FUN = mean)
+print("Permutation aggregate for size = 250")
+permutation_aggregate_250
 
-# Aggregate by initial and calculate mean cost and time
-initial_aggregate <- aggregate(cbind(cost, time) ~ initial, data = results, FUN = mean)
-initial_aggregate
+# Aggregate by initial and calculate mean cost and time for size = 250
+initial_aggregate_250 <- aggregate(cbind(relative_deviation, time) ~ initial, data = subset(results, size == 250), FUN = mean)
+initial_aggregate_250
+
+# Aggregate by permutation and calculate mean cost and time for size = 150
+permutation_aggregate_150 <- aggregate(cbind(relative_deviation, time) ~ permutation, data = subset(results, size == 150), FUN = mean)
+print("Permutation aggregate for size = 250")
+permutation_aggregate_150
+
+# Aggregate by initial and calculate mean cost and time for size = 150
+initial_aggregate_150 <- aggregate(cbind(relative_deviation, time) ~ initial, data = subset(results, size == 150), FUN = mean)
+initial_aggregate_150
