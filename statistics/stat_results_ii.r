0391bbfa2ead73a585eb6398b7b86f5bce5c0f89
Exagor <alexandre.achten@gmail.com>
2024-04-05 18:17:17 +0200

    Add standard deviation in statistics

--- statistics/stat_results_ii.r
+++ statistics/stat_results_ii.r
@@ -1,5 +1,5 @@
 #Script to make general statistics about raw data
-results <- read.csv("raw_results/results_best.csv", header = TRUE)
+results <- read.csv("raw_results/results_first.csv", header = TRUE)
 
 algo <- "Best"
 results_ex_rd <- subset(results, permutation == "exchange" & initial == "random")
@@ -10,37 +10,56 @@ results_tr_rd <- subset(results, permutation == "transpose" & initial == "random
 results_tr_cw <- subset(results, permutation == "transpose" & initial == "CW")
 
 #Create new table
-stat_results <- data.frame("algorithm" = character(), "average relative deviation" = numeric(), "average time" = numeric(), "size" = numeric())
+stat_results <- data.frame("algorithm" = character(), "average relative deviation" = numeric(), "average time" = numeric(), "size" = numeric(), "sd_rel_dev"= numeric(), "sd_time" = numeric())
 
 aggregate_results <- aggregate(cbind(relative_deviation, time) ~ size, data = results_ex_rd, FUN = mean)
+aggregate_results_sd <- aggregate(cbind(relative_deviation, time) ~ size, data = results_ex_rd, FUN = sd)
 aggregate_results$algorithm <- paste(algo, "exchange random") #insert name of the algorithm
+aggregate_results$sd_relative_deviation <- aggregate_results_sd$relative_deviation
+aggregate_results$sd_time <- aggregate_results_sd$time
 stat_results <- rbind(stat_results, aggregate_results) #insert the row in the table
 
+
 aggregate_results <- aggregate(cbind(relative_deviation, time) ~ size, data = results_ex_cw, FUN = mean)
+aggregate_results_sd <- aggregate(cbind(relative_deviation, time) ~ size, data = results_ex_cw, FUN = sd)
 aggregate_results$algorithm <- paste(algo, "exchange CW")
+aggregate_results$sd_relative_deviation <- aggregate_results_sd$relative_deviation
+aggregate_results$sd_time <- aggregate_results_sd$time
 stat_results <- rbind(stat_results, aggregate_results)
 
 aggregate_results <- aggregate(cbind(relative_deviation, time) ~ size, data = results_in_rd, FUN = mean)
+aggregate_results_sd <- aggregate(cbind(relative_deviation, time) ~ size, data = results_in_rd, FUN = sd)
 aggregate_results$algorithm <- paste(algo, "insert random")
+aggregate_results$sd_relative_deviation <- aggregate_results_sd$relative_deviation
+aggregate_results$sd_time <- aggregate_results_sd$time
 stat_results <- rbind(stat_results, aggregate_results)
 
 aggregate_results <- aggregate(cbind(relative_deviation, time) ~ size, data = results_in_cw, FUN = mean)
+aggregate_results_sd <- aggregate(cbind(relative_deviation, time) ~ size, data = results_in_cw, FUN = sd)
 aggregate_results$algorithm <- paste(algo, "insert CW")
+aggregate_results$sd_relative_deviation <- aggregate_results_sd$relative_deviation
+aggregate_results$sd_time <- aggregate_results_sd$time
 stat_results <- rbind(stat_results, aggregate_results)
 
 aggregate_results <- aggregate(cbind(relative_deviation, time) ~ size, data = results_tr_rd, FUN = mean)
+aggregate_results_sd <- aggregate(cbind(relative_deviation, time) ~ size, data = results_tr_rd, FUN = sd)
 aggregate_results$algorithm <- paste(algo, "transpose random")
+aggregate_results$sd_relative_deviation <- aggregate_results_sd$relative_deviation
+aggregate_results$sd_time <- aggregate_results_sd$time
 stat_results <- rbind(stat_results, aggregate_results)
 
 aggregate_results <- aggregate(cbind(relative_deviation, time) ~ size, data = results_tr_cw, FUN = mean)
+aggregate_results_sd <- aggregate(cbind(relative_deviation, time) ~ size, data = results_tr_cw, FUN = sd)
 aggregate_results$algorithm <- paste(algo, "transpose CW")
+aggregate_results$sd_relative_deviation <- aggregate_results_sd$relative_deviation
+aggregate_results$sd_time <- aggregate_results_sd$time
 stat_results <- rbind(stat_results, aggregate_results)
 
 # Formatting the column
 names(stat_results)[names(stat_results) == "relative_deviation"] <- "average_relative_deviation"
 names(stat_results)[names(stat_results) == "time"] <- "average_time"
 
-column_order <- c("algorithm", "size", "average_relative_deviation", "average_time")
+column_order <- c("algorithm", "size", "average_relative_deviation","sd_relative_deviation", "average_time", "sd_time")
 
 stat_results <- stat_results[, column_order]
 #Save the table
--- statistics/stat_results_vnd.r
+++ statistics/stat_results_vnd.r
@@ -6,14 +6,20 @@ results_vnd1 <- subset(results, permutation == "VND1")
 results_vnd2 <- subset(results, permutation == "VND2")
 
 #Create new table
-stat_results <- data.frame("algorithm" = character(), "average relative deviation" = numeric(), "average time" = numeric(), "size" = numeric())
+stat_results <- data.frame("algorithm" = character(), "average relative deviation" = numeric(), "average time" = numeric(), "size" = numeric(), "sd_rel_dev"= numeric(), "sd_time" = numeric())
 
 aggregate_results <- aggregate(cbind(relative_deviation, time) ~ size, data = results_vnd1, FUN = mean)
+aggregate_results_sd <- aggregate(cbind(relative_deviation, time) ~ size, data = results_vnd1, FUN = sd)
 aggregate_results$algorithm <- paste(algo, "1") #insert name of the algorithm
+aggregate_results$sd_relative_deviation <- aggregate_results_sd$relative_deviation
+aggregate_results$sd_time <- aggregate_results_sd$time
 stat_results <- rbind(stat_results, aggregate_results) #insert the row in the table
 
 aggregate_results <- aggregate(cbind(relative_deviation, time) ~ size, data = results_vnd2, FUN = mean)
+aggregate_results_sd <- aggregate(cbind(relative_deviation, time) ~ size, data = results_vnd2, FUN = sd)
 aggregate_results$algorithm <- paste(algo, "2")
+aggregate_results$sd_relative_deviation <- aggregate_results_sd$relative_deviation
+aggregate_results$sd_time <- aggregate_results_sd$time
 stat_results <- rbind(stat_results, aggregate_results)
 
 
@@ -21,7 +27,7 @@ stat_results <- rbind(stat_results, aggregate_results)
 names(stat_results)[names(stat_results) == "relative_deviation"] <- "average_relative_deviation"
 names(stat_results)[names(stat_results) == "time"] <- "average_time"
 
-column_order <- c("algorithm", "size", "average_relative_deviation", "average_time")
+column_order <- c("algorithm", "size", "average_relative_deviation","sd_relative_deviation", "average_time", "sd_time")
 
 stat_results <- stat_results[, column_order]
 #Save the table
